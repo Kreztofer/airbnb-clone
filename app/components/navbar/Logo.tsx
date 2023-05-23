054ddff0e7cd642a9b289f57fa89e9bfacafdfd0
dominic-amalu <dominique.amalu@gmail.com>
2023-05-23 14:54:16 +0100

    first commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,19 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const Logo = () => {
+  const router = useRouter();
+  return (
+    <Image
+      onClick={() => router.push('/')}
+      height="100"
+      width="100"
+      className="hidden md:block cursor-pointer"
+      alt="logo"
+      src="/images/logo.png"
+    />
+  );
+};
+
+export default Logo;
